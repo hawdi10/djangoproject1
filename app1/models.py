4b71b50fe2a4733b4180ff6cc174d94f29f2409c
hadi <hadiasadi7812@gmail.com>
2023-04-27 20:09:15 +0430

    add model and change admin

--- app1/models.py
+++ app1/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class person(models.Model):
-    name=models.CharField(max_length=8)
-    lname=models.CharField(max_length=8)
-    img=models.ImageField()
-    age=models.IntegerField()
-    date=models.DateField()
+    name=models.CharField(max_length=8,verbose_name="نام")
+    lname=models.CharField(max_length=8,verbose_name="نام خانوادگی")
+    img=models.ImageField(verbose_name="عکس")
+    age=models.IntegerField(verbose_name="سن")
+    date=models.DateField(verbose_name="عکس")
