d6b44643f6a89e8805c8c13f502eedcf8c6a45ec
hadi <hadiasadi7812@gmail.com>
2023-05-04 20:59:47 +0430

    add html file to templates

--- app1/models.py
+++ app1/models.py
@@ -4,9 +4,8 @@ from django.db import models
 class person(models.Model):
     name=models.CharField(max_length=8,verbose_name="نام")
     lname=models.CharField(max_length=8,verbose_name="نام خانوادگی")
-    img=models.ImageField(verbose_name="عکس")
-    age=models.IntegerField(verbose_name="سن")
-    date=models.DateField(verbose_name="عکس")
+    code_melli=models.IntegerField(verbose_name="کد ملی")
+
     
     def __str__(self):
         return f'{self.name}'
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app1/', include('app1.urls') )
+    path('app1/', include('app1.urls')),
 ]
